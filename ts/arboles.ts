f8f0d245ae6be97334981f0a6157aca0c57eff00
j4dev <jesusguanga98@outlook.com>
2019-11-03 19:55:27 -0500

    Buscar nodo codificado.

--- ts/arboles.ts
+++ ts/arboles.ts
@@ -93,7 +93,68 @@ p     */
             this._Trabajo.Hermano = temp;
 
             return temp;
-
         }
+    }
+
+    /**
+     * transversaPre
+     */
+    public transversaPre(nodo: clsNodo) {
+        let espacios = ""
+        var lista = "<li>Ábrol vacio</li>";
+        if (nodo == null) {
+            return lista;
+        }
+        for (let n = 0; n < this.i; n++) {
+            // El ciclo for solo se usa para dar espacios en la interfaz
+            espacios = espacios+"&nbsp";
+            lista = "<li>"+espacios+nodo.Dato+"</li>";
+        }
+
+        //Recorremos al hijo en transversa preorden
+        if (nodo.Hijo != null) {
+            this.i++;
+            this.transversaPre(nodo.Hijo);
+            this.i--;
+        }
+
+        //Si existen hermanos seran procesados
+        if (nodo.Hermano != null) {
+            this.transversaPre(nodo.Hermano);
+        }
+    }
+
+    /**
+     * buscarNodo  
+     */
+    public buscarNodo(dato:string, nodo:clsNodo):clsNodo {
+        var encontrado:clsNodo = null;
+
+        if (nodo == null) {
+            return encontrado;
+        }
+
+        if (nodo.Dato == dato) {
+            encontrado = nodo;
+            return encontrado;
+        }
+
+        //Buscamos en el hijo
+        if (nodo.Hijo != null) {
+            encontrado = this.buscarNodo(dato,nodo.Hijo);
+            if (encontrado != null) {
+                return encontrado;
+            }
+        }
+
+        //Buscamos en los hermanos
+        if (nodo.Hermano != null) {
+            encontrado = this.buscarNodo(dato, nodo.Hermano);
+            if (encontrado != null) {
+                return encontrado;
+            }
+        }
+
+        return encontrado;
     }
 }
